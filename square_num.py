b98926a2dd1d7aef00d5ceee586d2c7ade3cfd77
shamanaz12 <160182090+shamanaz12@users.noreply.github.com>
2025-04-04 19:39:14 +0500

    1

---
+++ square_num.py
@@ -0,0 +1,7 @@
+def main():
+    num: float = float(input("Type a number to see its square: "))
+    square: float = num ** 2
+    print(str(num) + " squared is " + str(square))
+
+if __name__ == '__main__':
+    main()
